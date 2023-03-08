3979d2957ee3279f1a5971002ccd0f43641d752a
roost <roostamol@gmail.com>
2023-03-08 06:35:40 +0300

    add file

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 from kivy.uix.label import Label
 from kivy.uix.image import Image
-from datetime import datetime
 
 
 class app(App):
